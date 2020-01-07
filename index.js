65d66da2f73ec8cb08f467616f73f9fc6e8d17a5
vishweswaran <vishweswaran.p@ionixxtech.com>
2020-01-07 15:18:08 +0530

    add deployment script

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ app.get("/", function (req, res) {
  res.send("Hello world");
 });
 //listen to port 3000 by default
-app.listen(process.env.PORT || 3000);
+app.listen(process.env.PORT || 8000);
  
 module.exports = app;
 
