8ba87c98ab5b03b77227de153c4754592c32a345
vishweswaran <vishweswaran.p@ionixxtech.com>
2020-01-07 14:12:33 +0530

    Simple node app with test

---
+++ test/test.js
@@ -0,0 +1,8 @@
+var request = require("supertest");
+var app = require("../index.js");
+describe("GET /", function() {
+ it("respond with Hello world", function(done) {
+ //navigate to root and check the the response is "hello world"
+ request(app).get("/").expect("Hello world", done);
+ });
+});
