293db5466aefaf021645b8711b16b8bc0749d207
vishweswaran <vishweswaran.p@ionixxtech.com>
2020-01-07 16:15:31 +0530

    Chnaged deploy script

--- test/test.js
+++ test/test.js
@@ -3,6 +3,6 @@ var app = require("../index.js");
 describe("GET /", function() {
  it("respond with Hello world", function(done) {
  //navigate to root and check the the response is "hello world"
- request(app).get("/").expect("Hello world", done);
+ request(app).get("/").expect("Hello from Jenkins", done);
  });
 });
