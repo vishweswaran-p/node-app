6d4fd69685e38ffebab097e8232bf62e815c127b
vishweswaran <vishweswaran.p@ionixxtech.com>
2020-01-09 12:48:39 +0530

    Chnages

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ var express = require("express");
 var app = express();
 //Respond with "hello world" for requests that hit our root "/"
 app.get("/", function (req, res) {
+  console.log("Request received"):
  res.send("Hello from Jenkins");
 });
 //listen to port 3000 by default
